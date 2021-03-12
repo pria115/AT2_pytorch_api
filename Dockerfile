c8760ff9774dd1cd29d9f438569024ab1b7fba21
Priya Srini <priyanka.srinivasa@student.uts.edu.au>
2021-03-12 21:48:13 +1100

    init

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.7
+
+COPY requirements.txt .
+
+RUN pip install --upgrade pip
+
+RUN pip3 install -r requirements.txt
+
+RUN pip install torch==1.7.1+cpu -f https://download.pytorch.org/whl/torch_stable.html
+
+COPY ./app /app
+
+COPY ./models /models
+
+CMD ["gunicorn", "-k", "uvicorn.workers.UvicornWorker", "-c", "/gunicorn_conf.py", "main:app"]
---
+++ app/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from starlette.responses import JSONResponse
+from joblib import load
+import torch
+import pandas as pd
+import os
+import sys
+sys.path.append(os.path.abspath('.'))
+
+app = FastAPI()
+
+pytorch = torch.load('models/pytorch_beer_style.joblib')
+
+@app.get("/")
+def read_root():
+    return {"Hello": "BeerFans"}
+
+@app.get('/health', status_code=200)
+def healthcheck():
+    return 'Get ready to taste the best beer!!'
+
+
+def format_features(brewery_name: str,	review_aroma: int, review_appearance: int, review_palate: int, review_taste: int):
+  return {
+        'Brewery': [brewery_name],
+        'Aroma': [review_aroma],
+        'Appearance': [review_appearance],
+        'Palate': [review_palate],
+	'Taste": [review_taste]
+    }
+
+
+@app.get("/mall/customers/segmentation")
+def predict(brewery_name: str,	review_aroma: int, review_appearance: int, review_palate: int, review_taste: int):
+    features = format_features(brewery_name, review_aroma, review_appearance, review_palate, review_taste)
+    obs = pd.DataFrame(features)
+    pred = pytorch_beer_style.predict(obs)
+    return JSONResponse(pred.tolist())
