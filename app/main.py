421de2623d4acea38b5af2dfe3864a90a28635b5
Priya Srini <priyanka.srinivasa@student.uts.edu.au>
2021-03-13 15:19:30 +1100

    state_dict

--- app/main.py
+++ app/main.py
@@ -7,31 +7,12 @@ import pandas as pd
 
 app = FastAPI()
 
-def get_device():
-    if torch.cuda.is_available():
-        device = torch.device('cuda:0')
-    else:
-        device = torch.device('cpu') # don't have GPU 
-    return device
-
-class PytorchMultiClass(nn.Module):
-    def __init__(self, num_features):
-        super(PytorchMultiClass, self).__init__()
-        
-        self.layer_1 = nn.Linear(num_features, 32)
-        self.layer_out = nn.Linear(32, 104)
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, x):
-        x = F.dropout(F.relu(self.layer_1(x)), training=self.training)
-        x = self.layer_out(x)
-        return self.softmax(x)
-
 import os
 import sys
 sys.path.append(os.path.abspath('.'))
 
-pytorch = torch.load('../models/pytorch_beer_style.pt', encoding='ascii')
+
+pytorch = torch.load('../models/pytorch_beer.pt', encoding='ascii')
 
 
 @app.get("/")
