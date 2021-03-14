75162aec11d0387b09e972bcae56917d2cb07075
Priya Srini <priyanka.srinivasa@student.uts.edu.au>
2021-03-14 12:45:57 +1100

    new

--- app/main.py
+++ app/main.py
@@ -48,23 +48,5 @@ def get_device():
 def predict(brewery_name: str,	review_aroma: int, review_appearance: int, review_palate: int, review_taste: int):
     features = format_features(brewery_name, review_aroma, review_appearance, review_palate, review_taste)
     obs = pd.DataFrame(features)
-
-    
-    sc = StandardScaler()
-    num_cols = ['review_palate', 'review_aroma', 'review_taste', 'review_appearance']
-    obs[num_cols] = sc.fit_transform(obs[num_cols])
-    
-    cat_cols = ['brewery_name']
-    encoder = ce.BinaryEncoder(cols=cat_cols)
-    obs = encoder.fit_transform(obs)
-
-    df_tensor= torch.Tensor(np.array(obs)).to(device)
-
-    device = get_device()
-    df_tensor= torch.Tensor(np.array(obs)).to(device)
-
-    output = pytorch(df_tensor)
-
-    pred = pytorch.predict(df_tensor)
-
+    pred = pytorch.predict(obs)
     return JSONResponse(pred.tolist())
